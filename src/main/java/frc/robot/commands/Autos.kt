4cd70ef9b1ccc678356263b631e7933e605c37c3
Boomrbam <Xashley2007@gmail.com>
2024-11-22 08:33:47 -0600

    Code

--- src/main/java/frc/robot/commands/Autos.kt
+++ src/main/java/frc/robot/commands/Autos.kt
@@ -4,19 +4,16 @@ import com.pathplanner.lib.auto.AutoBuilder
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands
-import edu.wpi.first.wpilibj2.command.PrintCommand
-import frc.robot.subsystems.ExampleSubsystem
 
 object Autos
 {
-    val autoModeChooser = AutoBuilder.buildAutoChooser()
+    private val autoModeChooser: SendableChooser<Command> = AutoBuilder.buildAutoChooser()
 
     init{
         SmartDashboard.putData("Auto Chooser", autoModeChooser)
     }
 
-    public fun getAutonomousCommand(): Command{
+    fun getAutonomousCommand(): Command{
         return autoModeChooser.selected
     }
 
